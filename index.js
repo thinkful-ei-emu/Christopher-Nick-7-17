c877d8f1f6e13d50b99b0c6a6acfd69b7db8c653
Chris Martin <chris.bizunit34@gmail.com>
2019-07-17 15:54:54 -0400

    added testing for Google Play Apps

--- index.js
+++ index.js
@@ -17,22 +17,21 @@ server.get('/apps', (req, res) => {
   let filteredApps = [...apps];
   //right now condition is if object has property sort and sort does not equal rating or app
   if (req.query.hasOwnProperty('sort') && ((sort !== 'rating' ) && (sort !== 'app' ))){
-    return res.status(400).json({error: "Sort must be 'rating' or 'app'"})
+    return res.status(400).json({error: 'Sort must be \'rating\' or \'app\''});
   }
   if (sort){
     let convertedSort = titlize(sort);
     filteredApps.sort((a , b) => a[convertedSort] > b[convertedSort] ? 1 : -1);
   }
+
   if (req.query.hasOwnProperty('genres') && ((genres !== 'action' ) && (genres !== 'puzzle' ) && (genres !== 'strategy' ) && (genres !== 'casual' ) && (genres !== 'arcade') && (genres !== 'card'))){
-    return res.status(400).json({error: "Please enter valid genres, ex: action"})
+    return res.status(400).json({error: 'Please enter valid genres, ex: action'});
   }
 
   if(genres){
-    let convertedGenres = titlize(genres)
-    filteredApps= filteredApps.filter(app => app['Genres'] === convertedGenres)
-    console.log(filteredApps)
-    
-    
+    let convertedGenres = titlize(genres);
+    filteredApps= filteredApps.filter(app => app['Genres'] === convertedGenres);
+
   }
 
 
--- test/index.test.js
+++ test/index.test.js
@@ -18,18 +18,18 @@ describe('GET /apps', () => {
 
   it('should sort apps by rating or title', () => {
     const sortVals = ['Rating', 'App'];
-    sortVals.forEach(val => {
-      it(`should return 200 with ${val} sorted list`, () => {
+    sortVals.forEach(sort => {
+      it(`should return 200 with ${sort} sorted list`, () => {
         return request(server)
           .get('/apps')
-          .query(val)
+          .query({ sort })
           .expect(200)
           .then(res => {
             expect(res.body).to.be.an('array');
             let i = 1; 
             let correctOrder = true; 
             while ( correctOrder && i < res.body.length) { 
-              if (res.body[i - 1][val] > res.body[i][val]) {
+              if (res.body[i - 1][sort] > res.body[i][sort]) {
                 correctOrder = false; } 
               i++; } 
             expect(correctOrder).to.be.true;       
@@ -39,7 +39,32 @@ describe('GET /apps', () => {
   });
 
 
-  it('should be filterable by genre');
+  it('should be filterable by genre', () => {
+    let genreVals = ['action', 'puzzle', 'strategy', 'casual', 'arcade', 'card'];
+    genreVals.forEach(genres => {
+      it(`should be filtered by ${genres}`, () => {
+        return request(server)
+          .get('/apps')
+          .query({ genres })
+          .expect(200)
+          .then(res => {
+            expect(res.body).to.be.an('array');
+          });
+      });
+    });
+  });
 
-  it('should throw error for invalid query');
+  it('should throw error for invalid sort query', () => {
+    return request(server)
+      .get('/apps')
+      .query({ sort: 'cheese' })
+      .expect(400, {error: 'Sort must be \'rating\' or \'app\''});
+  });
+
+  it('should throw error for invalid genre query', () => {
+    return request(server)
+      .get('/apps')
+      .query({ genres: 'cheese' })
+      .expect(400, {error: 'Please enter valid genres, ex: action'});
+  });
 });
