a669d81e03385f063ec76f0a6d8bd67ece40a30b
Chris Martin <chris.bizunit34@gmail.com>
2019-07-17 12:47:52 -0400

    started the string count drill

---
+++ test/app.test.js
@@ -0,0 +1,21 @@
+const expect = require('chai').expect;
+const request = require('supertest');
+const app = require('../app');
+
+describe('GET /frequency', () => {
+  it('should return an object', () => {
+    return request(app)
+      .get('/frequency?s=zzzzbbccdddeeeffff')
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .then(res => {
+        expect(res.body).to.be.an('object');
+        expect(res.body).to.include.all.keys('unique', 'average', 'highest');
+        expect(res.body.unique).to.equal(6);
+        expect(res.body.average).to.equal(3);
+        expect(res.body.highest).to.equal('f');
+      });
+  });
+
+  it('should throw an error');
+});
